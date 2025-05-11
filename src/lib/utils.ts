9d1e8f6aa7f8edb1e5e5caef3743b67060fdc76b
Adrian Utomo <adrianhakimutomo@gmail.com>
2025-05-12 01:07:58 +0700

    chore: add utils file

---
+++ src/lib/utils.ts
@@ -0,0 +1,12 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function decodeHtmlEntities(text: string): string {
+  const textArea = document.createElement("textarea")
+  textArea.innerHTML = text
+  return textArea.value
+}
